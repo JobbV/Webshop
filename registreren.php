b2e0db183ba6db19cce6ed6760cdb2808863f9ac
Job <566351@edu.rocmn.nl>
2022-05-18 14:51:49 +0200

    registreren form aangemaakt/ SQL statement

--- registreren.php
+++ registreren.php
@@ -1,6 +1,6 @@
 <?php
 include "registreren.html";
-include "bibliotheek/mailen.php";
+// include "bibliotheek/mailen.php";
 include "DBConfig.php";
 
 if(isset($_POST["submit"])){
@@ -11,10 +11,38 @@ if(isset($_POST["submit"])){
     $straat = htmlspecialchars($_POST['straat']);
     $postcode = htmlspecialchars($_POST['postcode']);
     $woonplaats = htmlspecialchars($_POST['woonplaats']);
-    $email = htmlspecialchars($_POST['e-mail']);
+    $email = htmlspecialchars($_POST['email']);
     $wachtwoord = htmlspecialchars($_POST['wachtwoord']);
     $wachtwoordHash = password_hash($wachtwoord, PASSWORD_DEFAULT);
 
+    // Controleer of e-mail al bestaat (geen dubbele adressen)
+    $sql = "SELECT * FROM klant WHERE email = ?";
+    $stmt = $verbinding->prepare($sql);
+    $stmt->execute(array($email));
+    $resultaat = $stmt->fetch(PDO::FETCH_ASSOC);
+    if($resultaat){
+        $melding = "Dit e-mailadres is al geregistreerd.";
+    } else {
+        $sql = "INSERT INTO klant (ID, voornaam, achternaam, straat, postcode, woonplaats, email, wachtwoord, rol) values (null,?,?,?,?,?,?,?,?)";
+        $stmt = $verbinding->prepare($sql);
+        try {
+            $stmt->execute(array(
+                $voornaam,
+                $achternaam,
+                $straat,
+                $postcode,
+                $woonplaats,
+                $email,
+                $wachtwoordHash,
+            0)
+            );
+            $melding = "Nieuw account aangemaakt.";
+        }catch(PDOException $e) {
+            $melding = "Kon geen account aanmaken." .
+            $e->getMessage();
+        }
+        echo "<div id='melding'> $melding </div>";
+    }
 }
 
 ?>
